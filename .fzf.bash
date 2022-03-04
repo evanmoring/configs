5e1eb7250d145fcc0ad64f3ab2cdbb0e768098a3
Evan <evan@peanutrobotics.com>
2022-03-04 11:10:27 -0800

    Made fzf username agnostic

--- .fzf.bash
+++ .fzf.bash
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/evan-peanut/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/home/evan-peanut/.fzf/bin"
+if [[ ! "$PATH" == */home/$USERNAME/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/home/$USERNAME/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/evan-peanut/.fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "/home/$USERNAME/.fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/evan-peanut/.fzf/shell/key-bindings.bash"
+source "/home/$USERNAME/.fzf/shell/key-bindings.bash"
