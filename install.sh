1be5aad4d9b5f1716429d9c17c3450dcb106a283
evan <epmoring@gmail.com>
2024-08-03 14:34:13 -0700

    Added install script and remove .evan_rc

---
+++ install.sh
@@ -0,0 +1,6 @@
+sudo apt install vim -y
+sudo apt install tmux -y
+git submodule update --init --recursive
+sudo apt install fzf
+sudo apt install curl
+curl -o broot -L https://dystroy.org/broot/download/x86_64-linux/broot
